1921dd7c7794ff5a39fdd96a6cd974c5bff0f168
Diegoc04 <diego.fercho.castellanos@gmail.com>
2024-08-28 22:06:53 -0500

    Punto 1 terminado

--- wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Control.java
+++ wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -4,41 +4,70 @@
  * and open the template in the editor.
  */
 package edu.eci.arsw.primefinder;
+import java.util.Scanner;
 
-/**
- *
- */
 public class Control extends Thread {
-    
+
     private final static int NTHREADS = 3;
     private final static int MAXVALUE = 30000000;
     private final static int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
-
     private PrimeFinderThread pft[];
-    
+    private static final Object lock = new Object();
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for (i = 0; i < NTHREADS - 1; i++) {
+            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for (int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
+
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+
+                synchronized (lock) {
+                    for (PrimeFinderThread thread : pft) {
+                        thread.pauseThread();
+                    }
+
+                    // Mostrar el número de primos encontrados hasta el momento
+                    int totalPrimes = 0;
+                    for (PrimeFinderThread thread : pft) {
+                        totalPrimes += thread.getPrimes().size();
+                    }
+                    System.out.println("Primos encontrados hasta ahora: " + totalPrimes);
+
+                    System.out.println("Presiona ENTER para continuar...");
+                    new Scanner(System.in).nextLine();
+
+                    // Reanudar los hilos
+                    for (PrimeFinderThread thread : pft) {
+                        thread.resumeThread();
+                    }
+
+                    lock.notifyAll();
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
--- wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,66 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-                this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
-	}
+    int a, b;
+    private List<Integer> primes;
+    private static final Object lock = new Object();
+    private volatile boolean paused;
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
+    public PrimeFinderThread(int a, int b) {
+        super();
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+            synchronized (lock) {
+                // Verificar si el hilo está en pausa antes de continuar
+                while (paused) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                
+                if (isPrime(i)) {
                     primes.add(i);
                     System.out.println(i);
                 }
             }
-	}
-	
-	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
-            }
-	    return ans;
-	}
+        }
+    }
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
+    boolean isPrime(int n) {
+        boolean ans;
+        if (n > 2) {
+            ans = n % 2 != 0;
+            for (int i = 3; ans && i * i <= n; i += 2) {
+                ans = n % i != 0;
+            }
+        } else {
+            ans = n == 2;
+        }
+        return ans;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public void pauseThread() {
+        paused = true;
+    }
+
+    public void resumeThread() {
+        paused = false;
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+    }
 }
